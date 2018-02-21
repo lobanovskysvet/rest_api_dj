5dcf87ce97308ffbac1165dd94e97db3780cb1c0
lobanovskysvet <lobanovskysvet@gmail.com>
2018-02-21 15:15:37 +0200

    Changes: fixed sending emails

--- TaskApp/Serializers.py
+++ TaskApp/Serializers.py
@@ -2,24 +2,23 @@ from rest_framework import serializers
 
 from TaskApp.models import CustomUser
 from django.contrib.auth.hashers import make_password
+from .utils import send_email
 
 
 class UserAccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ('username', 'password')
+        fields = ('username', 'email', 'password')
 
     def create(self, validated_data):
         user = CustomUser(**validated_data)
+        send_email(validated_data['email'], 'Registration', 'Hi 123')
         user.password = make_password(validated_data['password'])
         user.save()
         return user.id
-
-
 
 
 class UserViewSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ('id', 'first_name', 'last_name', 'email', 'country')
-
